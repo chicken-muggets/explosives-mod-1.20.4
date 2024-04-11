087e352b241bc849e390bbea368003553108d022
chicken-muggets <055572@atc.qld.edu.au>
2024-04-11 12:17:41 +1000

    Changed blocks

--- src/main/java/com/verbiox/block/ModBlocks.java
+++ src/main/java/com/verbiox/block/ModBlocks.java
@@ -4,28 +4,34 @@ import com.verbiox.ExplosivesMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.function.BooleanBiFunction;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 import java.util.stream.Stream;
 
+
 public class ModBlocks {
     public static final Block LAND_MINE = registerBlock("land_mine", new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).nonOpaque()));
-    private static final VoxelShape SHAPE = Stream.of(
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+    protected static final VoxelShape SHAPE = Stream.of(
             Block.createCuboidShape(1, 0, 1, 15, 1, 15),
             Block.createCuboidShape(3, 1, 3, 13, 2, 13),
             Block.createCuboidShape(4, 2, 5, 12, 3, 11)
     ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
-    public VoxelShape getOutlineShape() {
-        return SHAPE;
-    }
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, new Identifier(ExplosivesMod.MOD_ID, name), block);
