f71fb752842c5b9bdb8b3fb695bb6b69ad0483b7
chicken-muggets <055572@atc.qld.edu.au>
2024-04-12 17:32:21 +1000

    Added landmines fixed landmine hitbox made landmine explode when steped on

--- src/main/java/com/verbiox/block/ModBlocks.java
+++ src/main/java/com/verbiox/block/ModBlocks.java
@@ -7,6 +7,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
@@ -17,17 +18,16 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
 
 import java.util.stream.Stream;
 
 
 public class ModBlocks {
-    public static final Block LAND_MINE = registerBlock("land_mine", new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).nonOpaque()));
-    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return SHAPE;
-    }
+    public static final Block LAND_MINE = registerBlock("land_mine", new CustomBlock(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).nonOpaque()));
 
-    protected static final VoxelShape SHAPE = Stream.of(
+    public static final VoxelShape SHAPE = Stream.of(
             Block.createCuboidShape(1, 0, 1, 15, 1, 15),
             Block.createCuboidShape(3, 1, 3, 13, 2, 13),
             Block.createCuboidShape(4, 2, 5, 12, 3, 11)
@@ -42,5 +42,24 @@ public class ModBlocks {
     }
     public static void registerModBlocks() {
         ExplosivesMod.LOGGER.info("registering mod blocks for " + ExplosivesMod.MOD_ID);
+    }
+}
+class CustomBlock extends Block {
+    public CustomBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return ModBlocks.SHAPE;
+    }
+    @Override
+    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        System.out.println("Entity stepped on land mine");
+        // This code will be executed when the block is stepped on
+        if (!world.isClient) { // Ensure this is server side
+            world.createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), 4, World.ExplosionSourceType.TNT);
+            world.breakBlock(pos, true);
+        }
     }
 }
